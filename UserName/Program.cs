e2dbe5bcea6f412e4653852942acf5e2f6c2cf52
Heets1 <140603108+Heets1@users.noreply.github.com>
2023-07-27 11:30:41 +0300

    UserName
    UserName

---
+++ UserName/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Денис";
+            string lastName = "Карпеев";
+            byte age = 24;
+            string email = "denio4141@gmail.com";
+            string ballProgramm = "6,1";
+            string ballMat = "4,2";
+            string ballFiz = "5,3";
+
+            string newPattern = "Имя: {0} \nФамилия: {1} \nДата рождения: {2} \nПочта: {3} \nБаллы по программированию: {4} \nБаллы по математики: {5} \nБаллы по физике: {6}";
+            Console.WriteLine(newPattern,
+                firstName,
+                lastName,
+                age,
+                email,
+                ballProgramm,
+                ballMat,
+                ballFiz);
+            Console.ReadKey();
+
+            double valueA = 6.1; double valueB = 4.2; double valueC = 5.3;
+            double valueD = valueA + valueB + valueC;
+            double valueS = 15.6; int valueQ = 3;
+            double valueL = valueS / valueQ;
+
+
+            Console.WriteLine("Колличество баллов:" + valueD);
+            Console.WriteLine("Средний балл:" + valueL);
+            Console.ReadKey();
+
+        }
+    }
+}
